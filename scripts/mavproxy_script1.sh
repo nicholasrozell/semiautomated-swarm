9d9187f040e9a98104ec352e494916861fd78e3b
Rakshit <rakshitdayal.allamraju@gmail.com>
2021-02-11 11:13:12 -0600

    added scripts to launch mavproxy

---
+++ scripts/mavproxy_script1.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mavproxy.py --master=udp:127.0.0.1:14550 --out=udpout:127.0.0.1:16550 --out=udp:127.0.0.1:16551
---
+++ scripts/mavproxy_script2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mavproxy.py --out=udp:127.0.0.1:14552 --master=udp:127.0.0.1:16550 --master=udp:127.0.0.1:16552
