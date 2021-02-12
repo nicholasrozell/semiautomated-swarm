f853820fb27f8a2db9e3ea0c0656118729f4e4ac
Rakshit <rakshitdayal.allamraju@gmail.com>
2021-02-11 18:58:47 -0600

    Update install_script.sh
    updated geographiclib to install script

--- scripts/install_script.sh
+++ scripts/install_script.sh
@@ -57,4 +57,8 @@ sleep 2s
 
 sudo apt install ros-noetic-mavros ros-noetic-mavros-msgs ros-noetic-mavros-extras
 
+sleep 1s
+wget https://raw.githubusercontent.com/mavlink/mavros/master/mavros/scripts/install_geographiclib_datasets.sh
+./install_geographiclib_datasets.sh
+
 printf "INSTALLED MAVROS"
