a6e11dca32eb2fb2047b8a53ac3c756940447809
Rakshit <rakshitdayal.allamraju@gmail.com>
2021-02-19 13:00:48 -0600

    admin permission to install geographiclibs

--- scripts/install_script.sh
+++ scripts/install_script.sh
@@ -59,7 +59,7 @@ sudo apt install ros-noetic-mavros ros-noetic-mavros-msgs ros-noetic-mavros-extr
 
 sleep 1s
 wget https://raw.githubusercontent.com/mavlink/mavros/master/mavros/scripts/install_geographiclib_datasets.sh
-./install_geographiclib_datasets.sh
+sudo ./install_geographiclib_datasets.sh
 
 printf "INSTALLED MAVROS"
 
