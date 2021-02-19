84a44b37f5f7c5b243d4596b1ac6e06ef4af16f9
Rakshit <rakshitdayal.allamraju@gmail.com>
2021-02-19 16:06:56 -0600

    removed passing sysID check here

--- scripts/startServices.sh
+++ scripts/startServices.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]
-  then
-    echo "System ID argument is required"
-exit 1
-fi
-
 cd ../services
 sudo cp *.service /etc/systemd/system
 
 sleep 1s
+sudo systemctl daemon-reload
+
+sleep 1s
 sudo systemctl start Mavproxy_Pix.service
+printf "Starting Mavproxy link from Pixhawk\n"
 
 sleep 1s
 sudo systemctl start Mavproxy_Comm.service
+printf "Starting Mavproxy link from Comm device\n"
 
 sleep 1s
-sudo systemctl start MavROS@$1.service
+sudo systemctl start MavROS.service
+printf "Starting Mavros service\n"
