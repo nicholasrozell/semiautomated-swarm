e1e01754f486653f61f93ee2de6d0ff1678df897
Nicholas Rozell <nicholasrozell@users.noreply.github.com>
2021-02-23 12:57:32 -0600

    Add utils.py
    Initial utils code

---
+++ python/utils.py
@@ -0,0 +1,18 @@
+import math
+import numpy as np
+
+
+def dist(p, q):
+    """
+    Finds the Euclidean distance between two points.
+    """
+    return math.sqrt(sum([(a - b)**2 for a, b in zip(p, q)]))
+
+
+def angle(a, b):
+    """
+    Finds the angle between the horizontal and two points.
+    """
+    x1, y1, z1 = a
+    x2, y2, z2 = b
+    return math.atan2(y2 - y1, x2 - x1)
