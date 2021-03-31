3ab75dc06cf134274dc2fefe3a47a89039b2f6db
Nicholas Rozell <72842012+nicholasrozell@users.noreply.github.com>
2021-03-31 16:38:10 -0500

    Added drawing function for sim results

--- python/views.py
+++ python/views.py
@@ -38,3 +38,41 @@ def draw(graph, s, g, path=None, enodes=[], leaves=[]):
     # plt.show(block=False)
     # plt.pause(1)
     # plt.close()
+
+def draw2(graph, s, g, path=None, leaves=None):
+    """
+    Plots the graph.
+    """
+    plt.figure(figsize=[9, 9])
+    plt.xlim(graph.span[0][0], graph.span[0][1])
+    plt.ylim(graph.span[1][0], graph.span[1][1])
+    plt.grid(linestyle='-.')
+    for n in graph._node:
+        x, y, z = n
+        plt.scatter(x, y, s=2, c='#7FFF00', zorder=2)
+    for e in graph._edge:
+        x = ([e[0][0], e[1][0]])
+        y = ([e[0][1], e[1][1]])
+        plt.plot(x, y, c='#00008B', lw='0.5', zorder=1)
+    if path is not None:
+        for i in range(len(path)):
+            # print(i)
+            for j in range(len(path[i])-1):
+                # print(j)
+                x = ([path[i][j][0], path[i][j+1][0]])
+                y = ([path[i][j][1], path[i][j+1][1]])
+                plt.plot(x, y, c='#0000FF', lw='0.5', zorder=2)
+                if j == 0:
+                    plt.scatter(x, y, s=5, c='#FF0000', zorder=3)
+    if leaves != None:
+        for leaf in leaves:
+            x, y, z = leaf
+            plt.scatter(x, y, s=2, c='#008000', zorder=2)
+    if graph.obstacles != []:
+        for o in graph.obstacles:
+            x, y = list(o.exterior.xy)
+            plt.plot(x, y, c='#000000', zorder=1)
+    plt.scatter(s[0], s[1], s=50, c='#000000', marker='s', zorder=0)
+    plt.scatter(s[0], s[1], s=40, c='#FFFF00', marker='s', zorder=0)
+    plt.scatter(g[0], g[1], s=100, c='#FF00FF', marker='s', zorder=0)
+    plt.show()
