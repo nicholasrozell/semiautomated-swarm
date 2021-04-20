0a1db06d05ad529d25b54413edbdf6615b70c2bf
Nicholas Rozell <72842012+nicholasrozell@users.noreply.github.com>
2021-04-20 13:29:08 -0500

    Update test.py

--- python/test.py
+++ python/test.py
@@ -1,7 +1,43 @@
+import numpy as np
+import time
+
 from graph import Graph
-from rrt import MiniRRTStar
+from rrt import MiniRRTStar as RRT
 from views import draw
+from utils import dist
 
 
 def main():
-    pass
+    print('\nInitializing\n')
+    #                   x         y           z
+    dims = np.array([(0, 120), (0, 120), (-10, -10)])
+    init = (110, 10, -10)
+    goal = (20, 100, -10)
+    delta = 7
+    k = 3
+    
+    graph = Graph(dims)
+    path = None
+    PATH = []
+    t1 = time.time()
+
+    print('Calculating Trajectory...\n')
+    while 1:
+        rrt = RRT(graph, init, goal, delta, k, path)
+
+        path, leaves = rrt.search()
+        PATH.append(path)
+
+        if dist(init, goal) <= delta:
+            print('Goal Reached.')
+            print('Search Time :  {} s\n'.format(round(time.time() - t1, 3)))
+            print('Generating plot...\n')
+            draw(graph, init, goal, PATH, leaves)
+            break
+
+        graph.clear()
+        init = tuple(path[1])
+
+if __name__=='__main__':
+    main()
+    print('TERMINATED')       
