f41a050fff4f67220a5891101b591eaf72badd84
Nicholas Rozell <72842012+nicholasrozell@users.noreply.github.com>
2021-05-03 17:01:56 -0500

    Update flight_test.py

--- python/flight_test.py
+++ python/flight_test.py
@@ -83,13 +83,13 @@ class PathPlanning:
         rate.sleep()
 
         dims = np.array([(-500, 2500), (-100, 2900), (-50, -50)])
-        # obstacle = [(Point(1200, 1000).buffer(300))]
+        obstacle = [(Point(1400, 1200).buffer(300))]
         init = self.pos
         goal = (2300.0, 2600.0, -50.0)
         delta = 100
         k = 2
 
-        graph = Graph(dims)
+        graph = Graph(dims, obstacle)
         path = None
         trail = []
         position = []
